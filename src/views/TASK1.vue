23b7d8347977437bb058da8970cc7e2e91735de7
NguyenVuMao <maonv.eledevo@gmail.com>
2023-11-28 17:41:34 +0700

    add task1.vue

---
+++ src/views/TASK1.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <a-tabs v-model:activeKey="activeKey">
+      <a-tab-pane v-for="ItemTabLarge in mock" :key="ItemTabLarge.idTabLarge" :tab="ItemTabLarge.nameTabLarge">
+        <a-tabs v-model:activeKey1="activeKey1">
+          <a-tab-pane v-for="ItemTabSmall in ItemTabLarge.DataTabLarge" :key="ItemTabSmall.idTabSmall"
+            :tab="ItemTabSmall.nameTabSmall">
+            <a-collapse v-model:activeKey="collapseKey">
+              <a-collapse-panel v-for="Collapse in ItemTabSmall.DataTabSmall" :key="Collapse.idContent"
+                :header="Collapse.nameCollapse" @click="check(Collapse.idContent)">
+                {{ Collapse.textContent }}
+              </a-collapse-panel>
+            </a-collapse>
+          </a-tab-pane>
+        </a-tabs>
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+
+<script setup >
+import { ref } from 'vue';
+import { mock } from './mockdata';
+const state = ref([]);
+const activeKey = ref();
+const activeKey1 = ref();
+const collapseKey = ref(state);
+
+function check(a) {
+  const filteredArray = state.value.filter((value) => {
+    if (value === a) {
+      return false;
+    }
+    return true;
+  });
+}
+</script>
+
+
